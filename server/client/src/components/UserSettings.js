e374dfdfe5a27010b8c6bf1ce59113c013431dde
echugreeva <e.chugreeva@gmail.com>
2023-08-27 19:09:31 +0300

    card border, userdata in settings

--- server/client/src/components/UserSettings.js
+++ server/client/src/components/UserSettings.js
@@ -1,65 +1,55 @@
 
 import { useState, useEffect } from 'react'
-import { collection, query, onSnapshot, where, getDoc,getDocs,  doc, get } from "firebase/firestore"
+import { collection, query, onSnapshot, where, getDoc, getDocs, doc, get } from "firebase/firestore"
 import { auth, db } from '../config/firebase'
+import { Typography, Box, Container } from '@mui/material'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from 'react-router-dom'
+import ErrorBoundary from "./ErrorBoundary";
+
 
 
 const UserSettings = () => {
     const [userData, setUserData] = useState({})
+    let navigate = useNavigate();
 
-    const getUserData = async (uid) => {
-        // try {
-        //     const usersRef = collection(db, 'users');
-        //     const snapshot = await usersRef.where('uid', '==', uid).get()
+    const [user, loading, error] = useAuthState(auth)
 
-        //     if (snapshot.empty) {
-        //         console.log('No matching documents.');
-        //         return;
-        //       }  
+    const getUserData = async () => {
+        const userDocRef = doc(db, `users/${auth.currentUser.uid}`)
+        try {
+            let data = await getDoc(userDocRef)
+            let dbData = data.data()
+            setUserData(dbData)
+        } catch (err) {
+            console.error(`Error updating document:`, err);
 
 
 
-
-        //       snapshot.forEach((doc) => {
-        //       setUserData( ...doc.data());
-        //     });
-
-        //     // Now userData contains the data for the specific user, ordered by 'created' field
-        //     console.log(userData);
-        //   } catch (error) {
-        //     console.error('Error fetching user data:', error);
-        //   }
-
-        // const docSnap = await getDoc(doc(db, "users", auth.currentUser.uid));
-        // if (docSnap.exists()) {
-        //     console.log("Document data:", docSnap.data());
-        // } else {
-        //     // doc.data() will be undefined in this case
-        //     console.log("No such document!");
-        // }
-
-        const q = query(
-            collection(db, "users"),
-            where("uid", "==", uid)
-        );
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            setUserData(doc.data())
-        });
+        }
     }
+
 
 
     useEffect(() => {
         const userId = auth.currentUser.uid;
         console.log(auth.currentUser)
-        getUserData(userId)
+        getUserData()
     }, [])
 
-    return (<>
-    <p>my age: {userData.age}</p>
-    </>)
+    useEffect(() => {
+        if (loading) return;
+        if (!user) return navigate("/");
+        getUserData()
+    }, [user, loading]);
+
+    return (
+        <Container maxWidth="sm" sx={{ mt: 8 }}>
+            <Typography>Name: {userData.name}</Typography>
+            <Typography>Email: {userData.name}</Typography>
+            <Typography>Age: {userData.age}</Typography>
+        </Container>
+    )
 }
 
 export default UserSettings
