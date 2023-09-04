4d8fbcedb8c4d71a0bc7fc8d795ff8e6cbd308f8
echugreeva <e.chugreeva@gmail.com>
2023-09-04 17:38:03 +0300

    form for profile settings

--- server/client/src/components/UserSettings.js
+++ server/client/src/components/UserSettings.js
@@ -1,18 +1,22 @@
 
 import { useState, useEffect } from 'react'
-import { collection, query, onSnapshot, where, getDoc, getDocs, doc, get } from "firebase/firestore"
+import { collection, query, onSnapshot, where, getDoc, getDocs, doc, get, updateDoc, Timestamp } from "firebase/firestore"
 import { auth, db } from '../config/firebase'
-import { Typography, Box, Container } from '@mui/material'
+import { Typography, Box, Container, Grid, TextField, Button } from '@mui/material'
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from 'react-router-dom'
 import ErrorBoundary from "./ErrorBoundary";
 import { onAuthStateChanged } from "firebase/auth";
 
 
+
+
+
 const UserSettings = () => {
     const [userData, setUserData] = useState({})
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [formData, setFormData] = useState({});
     let navigate = useNavigate();
 
     // const [user, loading, error] = useAuthState(auth)
@@ -28,7 +32,29 @@ const UserSettings = () => {
 
         }
     }
-
+    const handleChange = (event) => {
+        setUserData({
+            ...userData, [event.target.name]: event.target.value,
+        })
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (user && user.id) {
+            const userDocRef = doc(db, `users/${auth.currentUser.uid}`);
+            try {
+                await updateDoc(userDocRef, {
+                    ...formData,
+                });
+                console.log(`Document  updated successfully.`);
+            } catch (err) {
+                console.error(`Error updating document :`, err);
+            }
+        }
+    }
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setUser(user);
@@ -40,8 +66,8 @@ const UserSettings = () => {
 
 
     useEffect(() => {
-        if (user && user.uid) { 
-            
+        if (user && user.uid) {
+
             getUserData()
         }
 
@@ -58,9 +84,62 @@ const UserSettings = () => {
 
     return (
         <Container maxWidth="sm" sx={{ mt: 8 }}>
-            <Typography>Name: {userData.name}</Typography>
-            <Typography>Email: {userData.name}</Typography>
-            <Typography>Age: {userData.age}</Typography>
+            <Grid container direction="column" alignItems="center" spacing={2}>
+                <Grid item xs={12}>
+                    <Typography>Here is your profile data, feel free to update it</Typography>
+                    <TextField
+                        value={userData.name}
+                        // label="Name"
+                        name="name"
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        value={userData.age}
+                        // label="Age"
+                        name="age"
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        value={userData.email}
+                        //  label="Email"
+                        name="email"
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        value={userData.bio}
+                        //  label="Email"
+                        name="bio"
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={(e) => {
+                            handleSubmit(e)
+                            navigate('/profile')
+
+                        }}
+                        sx={{ // Set minimum width
+                            borderRadius: '20px',
+                            
+                        }}
+                    >
+                        Submit Changes
+                    </Button>
+
+                </Grid>
+                {/* <Typography>Name: {userData.name}</Typography>
+                <Typography>Email: {userData.name}</Typography>
+                <Typography>Age: {userData.age}</Typography> */}
+            </Grid>
         </Container>
     )
 }
